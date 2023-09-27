720dc766e7a89524c3f174fee8392b322c172d0a
Micah Ives <micbenives@gmail.com>
2023-09-26 20:51:32 -0500

    Add quiz questions

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,23 @@
 var timerElement = document.getElementById('countdown');
 
+const quizQuestions = [
+    {
+        question: "What HTML element is used to embed JavaScript code?",
+        options: ["<footer>", "<src>", "<script>", "<js>"],
+        answer: "<script>"
+    },
+    {
+        question: "Which of the following keywords is not used to declare a variable in JavaScript?",
+        options: ["var", "let", "const", "All options declare a variable"],
+        answer: "All options declare a variable"
+    },
+    {
+        question: "What of the following is not a primitive data type in JavaScript?",
+        options: ["object", "NULL", "String", "Boolean"],
+        answer: "object"      
+    }
+]
+
 
 function countdown() {
     var timeLeft = 60;
