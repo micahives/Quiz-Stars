d5210e244e135487e3f00673f801af0f8dee115f
Micah Ives <micbenives@gmail.com>
2023-09-26 21:20:18 -0500

    Fix button timer, add quiz content section

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,7 @@
 var timerElement = document.getElementById('countdown');
 
+var startButton = document.getElementById('startButton');
+
 const quizQuestions = [
     {
         question: "What HTML element is used to embed JavaScript code?",
@@ -12,12 +14,17 @@ const quizQuestions = [
         answer: "All options declare a variable"
     },
     {
-        question: "What of the following is not a primitive data type in JavaScript?",
+        question: "Which of the following is not a primitive data type in JavaScript?",
         options: ["object", "NULL", "String", "Boolean"],
         answer: "object"      
     }
 ]
 
+startButton.addEventListener('click', function() {
+    startButton.disabled = true;
+
+    countdown();
+});
 
 function countdown() {
     var timeLeft = 60;
@@ -30,10 +37,13 @@ function countdown() {
         } else {
 
             clearInterval(timeInterval);
+            // Want to add red font/box flashing around timer when it is the last 10 seconds
 
         }
 
 
     }, 1000);
 }
+
+// For loop through quiz content in js that renders a different set of questions each time a button is clicked
 
